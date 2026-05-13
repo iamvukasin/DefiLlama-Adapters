4a8db05ce7ee4f034f87a6114382d18dfb134c65
rohannero <rohanzarathustra@gmail.com>
2026-05-13 01:18:08 -0400

    add methodology

--- registries/uniswapV3.js
+++ registries/uniswapV3.js
@@ -1716,7 +1716,7 @@ const uniV3Configs = {
     lukso: { factory: '0xFce4C544f07E2ca758a179788fe56e6A2941E681', fromBlock: 7393687 },
   },
   'fluxflow-v3': {
-    methodology: '',
+    methodology: 'TVL counts liquidity locked in FluxFlow V3 pools on Fluent. PoolCreated events are read from the factory and reserves of each pool are summed.',
     fluent: { factory: '0x69Be606be7Fd2d27C8f9821329c748c77d24FF4f', fromBlock: 2189672}
   }
 }
